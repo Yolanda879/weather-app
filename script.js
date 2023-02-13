fbd06e00fcd153b4202c61c7a1c49123b9863c52
Yolanda879 <yolandangolomi17@gmail.com>
2023-02-13 13:16:44 +0200

    updated description on javascript

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showWeather(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
 }
 
 function searchCity(city) {
