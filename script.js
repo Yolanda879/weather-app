7a65d9e551f74bb4424d64fbd4c5139e20eca9c9
Yolanda879 <yolandangolomi17@gmail.com>
2023-02-13 14:17:23 +0200

    added weather icon

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function showWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
